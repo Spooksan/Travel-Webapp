38f36cd0b166585e650312443c87e8d11b936c5c
Spooksan <suryakumarm92@gmail.com>
2019-02-26 17:17:27 +0530

    All the remaining functionality
    Full update

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,31 +1,43 @@
 import React, { Component } from "react";
 import "./Tour.scss";
 export default class Tour extends Component {
+  state = {
+    showInfo: false
+  };
+
+  handleInfo = () => {
+    this.setState({
+      showInfo: !this.state.showInfo
+    });
+  };
+
   render() {
+    const { id, city, img, name, info } = this.props.tour;
+    const { removeTour } = this.props;
     return (
       <article className="tour">
         <div className="img-container">
-          <img src="" alt="" />
+          <img src={img} alt="" />
+
+          <span
+            className="close-btn"
+            onClick={() => {
+              removeTour(id);
+            }}
+          >
+            <i className="fas fa-window-close" />
+          </span>
         </div>
-        <span className="close- btn">
-          <i className="fas fa-window-close" />
-        </span>
         <div className="tour-info">
-          <h3>city</h3>
-          <h4>name</h4>
+          <h3>{city}</h3>
+          <h4>{name}</h4>
           <h5>
             info{" "}
             <span>
               <i className="fas fa-caret-square-down" />
             </span>
           </h5>
-          <p>
-            Mollit excepteur non duis pariatur esse occaecat consequat
-            exercitation reprehenderit aute officia. Lorem dolor id sunt id
-            eiusmod id mollit id incididunt exercitation est. Ipsum minim
-            adipisicing consequat in aliqua id cupidatat commodo commodo
-            deserunt. Non proident est reprehenderit aliqua voluptate anim.
-          </p>
+          {this.state.showInfo && <p>{info}</p>}
         </div>
       </article>
     );
